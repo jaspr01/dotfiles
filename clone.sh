7ad8c339561ce41f75c6b16d72578b3ef6ff064b
Jasper Verschuere <verschuere.jasper@gmail.com>
2023-08-23 19:53:00 +0200

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -6,10 +6,10 @@ SITES=$HOME/Sites
 
 # Bolt
 git clone git@github.com:BoltEnergy-BE/bolt-api.git $SITES/bolt-api
+git clone git@github.com:BoltEnergy-BE/bolt-cms.git $SITES/bolt-cms
 git clone git@github.com:BoltEnergy-BE/bolt-internal.git $SITES/bolt-internal
 git clone git@github.com:BoltEnergy-BE/bolt-portal.git $SITES/bolt-portal
-git clone git@github.com:BoltEnergy-BE/bolt-sales-portal.git $SITES/bolt-sales-portal
 git clone git@github.com:BoltEnergy-BE/bolt-website.git $SITES/bolt-website
 
-# Creative Shelter
-git clone git@github.com:CreativeShelter/dev.creativeshelter.git $SITES/dev.creativeshelter
+# Side projects
+git clone git@github.com:jaspr01/huyswerk-keirse.git $SITES/huyswerk-keirse
